9f7870f8d5db6bcfb65464de680911cd1dfadd06
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-01-22 10:54:05 +0600

    fix duplicate

--- internal/db/queries.go
+++ internal/db/queries.go
@@ -47,11 +47,13 @@ func (d *Database) CreateOrder(ctx context.Context, order Order) (int64, error) 
 }
 
 func (d *Database) GetOrder(ctx context.Context, vmcNo int64, orderNo string) (Order, error) {
-	query := `SELECT qr_type, paid, amount FROM orders WHERE order_no = $1 AND vending_machine_id = $2`
+	query := `SELECT qr_type, paid, amount 
+			JOIN vending_machines on vending_machines.id = orders.vending_machine_id 
+			FROM orders WHERE orders.order_no = $1 AND  vending_machines.no= $2`
 
 	var order Order
 
-	err := d.db.QueryRow(ctx, query, vmcNo, order).Scan(&order.QRType, &order.Paid, &order.Amount)
+	err := d.db.QueryRow(ctx, query, orderNo, vmcNo).Scan(&order.QRType, &order.Paid, &order.Amount)
 
 	return order, err
 }
