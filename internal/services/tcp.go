9dead69165c25823c0f60ecfe13aeacf51fe4172
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-02-08 09:52:30 +0600

    call back paydone

--- internal/services/tcp.go
+++ internal/services/tcp.go
@@ -139,7 +139,7 @@ func (t *TCPServer) HandleConnection(conn *net.TCPConn) {
 			}
 
 		}
-		buf := make([]byte, 295)
+		buf := make([]byte, 300)
 		n, err = conn.Read(buf)
 		if err != nil {
 			log.Println(err)
